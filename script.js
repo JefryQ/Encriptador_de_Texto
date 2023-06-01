2d17d99850467dc5bae4f9bfa22d126f29d8b342
Jefry Quejada <jefryqt18@gmail.com>
2023-05-31 23:28:48 -0500

    Se añadio mas personalidad a los botones, se agrego el boton de copiar

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const textArea = document.getElementById("txtCod");
 const message = document.getElementById("mensajeEjecutado");
 const copy = document.getElementById("copiar");
+const paste = document.getElementById("pegar");
 copy.style.display = "none"
+paste.style.display = "none"
 
 let alfabeto = {
     "a": "/\\", "b": "8", "c": "¢", "d": "?", "e": "€", "f": "ph", "g": "&", "h": "*^",
-    "i": "!", "j": ";", "k": "1<" , "l": "|", "m": "^^", "n": "^/" , "o": "Q",
-    "p": "|*", "q": "9", "r": "Я", "s": "§", "t": "+", "u": "v", "v": "\\/",
-    "w": "v²", "x": ")(", "y": "Ч", "z": "s"
+    "i": "!", "j": ";", "k": "1<" , "l": "*|", "m": "^^", "n": "^/" , "o": "Q",
+    "p": "|*", "q": "9", "r": "Я", "s": "§", "t": "+", "u": "]", "v": "\\/",
+    "w": "-", "x": ")(", "y": "Ч", "z": "s"
 }
-function ingresoTexto(){
 
-}
 function validarTexto(){
     let textoIngresado = textArea.value;
     let validador = textoIngresado.match(/^[a-z\s]*$/);
@@ -22,7 +22,6 @@ function validarTexto(){
             title: 'Recuerda solo puedes ingresar letras minúsculas y sin acento',
             text: 'Intentalo de nuevo!'
         })
-        //alert("Solo son permitidas letras minúsculas y sin acentos")
         //location.reload();
         return true;
     }
@@ -32,15 +31,11 @@ function validarTexto(){
             title: 'No has ingresado ningún carácter para encriptar',
             text: 'Intentalo de nuevo!'
         })
-        //alert("Solo son permitidas letras minúsculas y sin acentos")
         //location.reload();
     }
 }
 
 function encriptar(text){
-/*    if(text.length === 0){
-        return "No ingresaste ninguna letra para codificar"
-    }*/
     let textEncriptado = "";
     for(let i = 0; i < text.length; i++){
         let letra = text[i];
@@ -65,17 +60,21 @@ function btnEncriptar(){
 
 function btnDesencriptar(){
     message.value = desencriptar(textArea.value);
+    message.style.backgroundImage = "none"
     textArea.value = "";
+    copy.style.display = "block"
+
+
 }
 function desencriptar(textEncriptado) {
     let textoIngresado = textArea.value
+
     if(textoIngresado === "") {
         Swal.fire({
             icon: 'error',
             title: 'No has ingresado ningún carácter para desencriptar',
             text: 'Intentalo de nuevo!'
         })
-        //alert("Solo son permitidas letras minúsculas y sin acentos")
         //location.reload();
     }
     let textDesencriptado = "";
@@ -83,6 +82,7 @@ function desencriptar(textEncriptado) {
     for (let i = 0; i < textEncriptado.length; i++) {
         let caracter = textEncriptado[i];
         palabraEncriptada += caracter;
+
         let clave = Object.keys(alfabeto).find(
             key => alfabeto[key].toLowerCase() === palabraEncriptada.toLowerCase()
         );
@@ -95,11 +95,15 @@ function desencriptar(textEncriptado) {
             textDesencriptado += " ";
             palabraEncriptada = "";
         }
-    }
+
+    }message.style.backgroundImage = 'url("imagenes/Muneco.png")';
     return textDesencriptado;
+
+
 }
 console.log(desencriptar(textArea.value));
 function copiar(){
+
     let textoCopiado = message.value
     if(textoCopiado === "") {
         Swal.fire({
@@ -108,13 +112,16 @@ function copiar(){
             text: 'Intentalo de nuevo!'
         })
         copy.style.display = "none"
+        paste.style.display = "block"
         //alert("Solo son permitidas letras minúsculas y sin acentos")
         //location.reload();
+        message.style.backgroundImage = 'url("imagenes/Muneco.png")';
     }else {
         message.select();
         navigator.clipboard.writeText(message.value)
         message.value = "";
         copy.style.display = "none"
+        paste.style.display = "block"
         Swal.fire({
             position: 'top',
             /*position: 'top-end',
@@ -125,16 +132,26 @@ function copiar(){
             showConfirmButton: false,
             timer: 600
         });
+        message.style.backgroundImage = 'url("imagenes/Muneco.png")';
     }
+}
+
+function pegar() {
+    navigator.clipboard.readText()
+        .then((text) => {
+            textArea.value = text;
+        })
+        .catch((error) => {
+            console.error('Error al pegar el texto:', error);
+        });
+    paste.style.display = "none"
 }
 
 function mostrarInstrucciones() {
     const instrucciones = document.getElementById("instrucciones");
     if (instrucciones.style.display === "none") {
-        instrucciones.style.display = "block"; // Mostrar el párrafo de las instrucciones
+        instrucciones.style.display = "block";
     } else {
-        instrucciones.style.display = "none"; // Ocultar el párrafo de las instrucciones
+        instrucciones.style.display = "none";
     }
 }
-
-//console.log(encriptar("Holañ"));
